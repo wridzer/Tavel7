a1211acf7dceaef3e7f08842143862fda90d93ae
MrSinga <62131291+MrSynga@users.noreply.github.com>
2021-01-26 21:49:07 +0100

    coc

---
+++ GGJ2021/Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float SPEED = .5f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z + Physics.gravity;
+
+        controller.Move(move * SPEED * Time.deltaTime);
+    }
+}
