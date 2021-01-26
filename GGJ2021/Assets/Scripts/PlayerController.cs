c19bd06d6fbe6c01cc3bb40dc710288f00930866
wridzer <wridzer12@gmail.com>
2021-01-26 21:27:24 +0100

    Update PlayerController.cs

--- GGJ2021/Assets/Scripts/PlayerController.cs
+++ GGJ2021/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //sean is een pannenkoek
     }
 
     // Update is called once per frame
