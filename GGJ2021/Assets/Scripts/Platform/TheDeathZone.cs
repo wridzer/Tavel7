f2949a13a650d0c8f8749410de1becc86a2cf6a4
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2021-01-29 20:23:53 +0100

    Veel plezier met mijn code

---
+++ GGJ2021/Assets/Scripts/Platform/TheDeathZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheDeathZone : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player") { Player.transform.position = new Vector3(-5, -2, 0); }
+        else { print("je moeder"); }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
