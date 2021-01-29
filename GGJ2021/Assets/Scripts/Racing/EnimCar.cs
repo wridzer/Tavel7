3c43602702d88a4d470971bdd5300770b2eba7ea
wridzer <wridzer12@gmail.com>
2021-01-29 23:58:49 +0100

    tag

--- GGJ2021/Assets/Scripts/Racing/EnimCar.cs
+++ GGJ2021/Assets/Scripts/Racing/EnimCar.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class EnimCar : MonoBehaviour
 {
-    private GameObject Player;
     public float moveSpeed = 2f;
     public float rotSpeed = 5f;
     [SerializeField] private Vector2 playerLoc;
     private Rigidbody2D rb;
+    public GameObject corner0, corner1, corner2, corner3, corner4, corner5, corner6, corner7;
+    List<GameObject> corners;
+    private GameObject target;
 
     // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.Find("Player");
         rb = GetComponent<Rigidbody2D>();
+        target = corner0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerLoc = new Vector2(Player.transform.position.x, Player.transform.position.y);
+        playerLoc = new Vector2(target.transform.position.x, target.transform.position.y);
         rb.transform.position = Vector2.MoveTowards(transform.position, playerLoc, moveSpeed * Time.deltaTime);
 
-        Vector3 dir = Player.transform.position - transform.position;
+        Vector3 dir = target.transform.position - transform.position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg * rotSpeed;
         rb.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
