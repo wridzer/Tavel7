2181f8f54544b76ef160906f43d8ea629b694d7e
MrSinga <62131291+MrSynga@users.noreply.github.com>
2021-01-29 23:45:26 +0100

    PetGame

---
+++ GGJ2021/Assets/Scripts/PetGame/Buttons.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public int petScore, feedScore, playScore;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(feedScore == 5)
+        {
+            //fat
+        }
+        if (feedScore == 10)
+        {
+            //ever fatter
+        }
+        if (feedScore == 15)
+        {
+            //EVEN FATTER
+        }
+        if (feedScore == 20)
+        {
+            //EXPLOSION
+        }
+    }
+
+    public void pet()
+    {
+        Debug.Log("pet");
+        petScore++;
+    }
+    public void Feed()
+    {
+        Debug.Log("Feed");
+        feedScore++;
+    }
+    public void Play()
+    {
+        Debug.Log("Play");
+        playScore++;
+    }
+}
