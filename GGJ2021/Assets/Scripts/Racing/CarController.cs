f49ca57e0785be9a6522ae8e32c835fb9b28d72e
wridzer <wridzer12@gmail.com>
2021-01-29 23:46:12 +0100

    race game

---
+++ GGJ2021/Assets/Scripts/Racing/CarController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float moveSpeed = 10;
+    public float turnSpeed = 5;
+    public float brakeSpeed = 5;
+
+    public GameObject gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector2 moving = new Vector2(0, moveSpeed);
+            rb.AddRelativeForce(moving);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.angularVelocity += turnSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.angularVelocity += -turnSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+                rb.drag = 4;
+        } else { rb.drag = 2; }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Finish")
+        {
+            gm.GetComponent<GameManger>().StartTimer();
+        }
+    }
+}
---
+++ GGJ2021/Assets/Scripts/Racing/EnimCar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnimCar : MonoBehaviour
+{
+    private GameObject Player;
+    public float moveSpeed = 2f;
+    public float rotSpeed = 5f;
+    [SerializeField] private Vector2 playerLoc;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLoc = new Vector2(Player.transform.position.x, Player.transform.position.y);
+        rb.transform.position = Vector2.MoveTowards(transform.position, playerLoc, moveSpeed * Time.deltaTime);
+
+        Vector3 dir = Player.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg * rotSpeed;
+        rb.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
