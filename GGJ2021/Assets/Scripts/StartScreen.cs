2ffb740dfc793f22ee081f890037867b2fdd4e10
MrSinga <62131291+MrSynga@users.noreply.github.com>
2021-01-29 16:14:24 +0100

    StartScreen

---
+++ GGJ2021/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("3DWorld");
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("YEET");
+    }
+}
