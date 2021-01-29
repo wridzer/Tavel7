35d7f1212837ebf41e2b3fb9f5fb23386d32b07e
wridzer <wridzer12@gmail.com>
2021-01-29 18:45:00 +0100

    bumper cars

---
+++ GGJ2021/Assets/Scripts/EnimFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnimFollow : MonoBehaviour
+{
+    private GameObject Player;
+    public float moveSpeed = 2f;
+    public float rotSpeed = 5f;
+    [SerializeField] private Vector2 playerLoc;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLoc = new Vector2(Player.transform.position.x, Player.transform.position.y);
+        rb.transform.position = Vector2.MoveTowards(transform.position, playerLoc, moveSpeed * Time.deltaTime);
+        //rb.transform.LookAt(Player.transform);
+
+        Vector3 dir = Player.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
---
+++ GGJ2021/Assets/Scripts/MovementTwoD.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTwoD : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float moveSpeed = 10;
+    public float turnSpeed = 5;
+    public float hitPower = 10;
+    private Vector2 bumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bumping = new Vector2(0, -hitPower);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector2 moving = new Vector2(0, moveSpeed);
+            rb.AddRelativeForce(moving);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.angularVelocity += turnSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.angularVelocity += -turnSpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Debug.Log("BOTS");
+            rb.AddRelativeForce(bumping);
+            collision.GetComponent<Rigidbody2D>().AddRelativeForce(bumping);
+        }
+    }
+}
