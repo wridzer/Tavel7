393cf0f4dd62436cd334fce1187be7b8b591cb7f
MrSinga <62131291+MrSynga@users.noreply.github.com>
2021-01-30 17:29:58 +0100

    DatingSimUpdate

--- GGJ2021/Assets/Scripts/DatinSim/Dating.cs
+++ GGJ2021/Assets/Scripts/DatinSim/Dating.cs
@@ -6,16 +6,21 @@ using UnityEngine.UI;
 public class Dating : MonoBehaviour
 {
     public Image main;
-    public Image sean;
-    public Image youri;
-    public Image wridzer;
-    public Image joppe;
+    public Image Chadrick;
+    public Image Ullu;
+
+    public GameObject chad;
+    public GameObject UWU;
 
     public Text text1;
+    public Text text2;
+    public Text text3;
 
     void Start()
     {
         startFalse();
+
+        StartCoroutine(BeginScene());
     }
 
     void Update()
@@ -23,26 +28,69 @@ public class Dating : MonoBehaviour
         
     }
 
-    public IEnumerator NextText()
+    public void ChadRick()
     {
-        //tekst
+        StartCoroutine(ChadRickC());
+    }
+    public void UlluUWU()
+    {
+        StartCoroutine(UlluUWUC());
+    }
+
+    public IEnumerator BeginScene()
+    {
+        main.enabled = true;
+        text1.enabled = true;
 
         while (!Input.GetMouseButtonDown(0))
             yield return null;
 
-        //tekst
-
+        text1.enabled = false;
+        chad.SetActive(true);
+        UWU.SetActive(true);
         yield return null;
+
+        while (!Input.GetMouseButtonDown(0))
+            yield return null;
+    }
+
+    public IEnumerator ChadRickC()
+    {
+        chad.SetActive(false);
+        UWU.SetActive(false);
+        text2.enabled = true;
+
+        while (!Input.GetMouseButtonDown(0))
+            yield return null;
+
+        text3.enabled = true;
+        yield return null;
+
+        while (!Input.GetMouseButtonDown(0))
+            yield return null;
+    }
+    public IEnumerator UlluUWUC()
+    {
+        chad.SetActive(false);
+        UWU.SetActive(false);
+
+        while (!Input.GetMouseButtonDown(0))
+            yield return null;
     }
 
     public void startFalse()
     {
         main.enabled = false;
-        sean.enabled = false;
-        youri.enabled = false;
-        wridzer.enabled = false;
-        joppe.enabled = false;
+        Chadrick.enabled = false;
+        Ullu.enabled = false;
 
+        //buttons
+        chad.SetActive(false);
+        UWU.SetActive(false);
+
+        //tekst
         text1.enabled = false;
+        text2.enabled = false;
+        text3.enabled = false;
     }
 }
