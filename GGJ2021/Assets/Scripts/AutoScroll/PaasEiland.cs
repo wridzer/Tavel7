b633a991110d57387d62a427f7e8ae278dc77f0f
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2021-01-30 01:23:39 +0100

    Dit is klaar

---
+++ GGJ2021/Assets/Scripts/AutoScroll/PaasEiland.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaasEiland : MonoBehaviour
+{
+    public GameObject Railz;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Railz.GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
---
+++ GGJ2021/Assets/Scripts/AutoScroll/WonTheGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WonTheGame : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player") { print("Je ruikt naar natte hond, kun je alsjeblieft ergens anders zitten?"); }
+    }
+}
