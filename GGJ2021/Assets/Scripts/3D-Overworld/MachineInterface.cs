56a4d82ad55ce82a8ab4a8c93032d5535ce095f7
wridzer <wridzer12@gmail.com>
2021-01-30 18:32:20 +0100

    LOOP

---
+++ GGJ2021/Assets/Scripts/3D-Overworld/MachineInterface.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MachineInterface : MonoBehaviour
+{
+    public Scene minigame;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(minigame != null)
+        {
+            //start animation
+            string sceneName = minigame.name;
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
