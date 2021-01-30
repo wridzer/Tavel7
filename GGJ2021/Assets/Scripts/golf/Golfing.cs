28ffb0a3a2f2ae1451eab4ed87ae91953fc4fe19
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2021-01-30 05:44:39 +0100

    oh golf

---
+++ GGJ2021/Assets/Scripts/golf/Golfing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golfing : MonoBehaviour
+{
+    [SerializeField]
+    bool GolfHitCheck = false;
+        
+    private void OnMouseDown()
+    {
+        GolfHitCheck = true;
+    }
+
+    private void OnMouseUp()
+    {
+        if (GolfHitCheck = true)
+        {}
+        GolfHitCheck = false; 
+    }
+
+    void Update()
+    {
+        
+    }
+}
