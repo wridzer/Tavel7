9a071712131e0228c5f329b1124a302a2d856d50
wridzer <wridzer12@gmail.com>
2021-01-30 19:48:47 +0100

    cutscene

--- GGJ2021/Assets/Scripts/3D-Overworld/MachineInterface.cs
+++ GGJ2021/Assets/Scripts/3D-Overworld/MachineInterface.cs
@@ -10,14 +10,15 @@ public class MachineInterface : MonoBehaviour
     public Text outOfOrder;
     public Text interText;
 
+    public GameObject player, cutSceneCam;
+
     public void Update()
     {
         if (Input.GetKey(KeyCode.E))
         {
             if (minigame != 0)
             {
-                //start animation
-                SceneManager.LoadScene(minigame);
+                StartCoroutine(CutScene());
             }
             if (minigame == 0)
             {
@@ -34,5 +35,13 @@ public class MachineInterface : MonoBehaviour
         outOfOrder.GetComponent<Text>().enabled = true;
         yield return new WaitForSeconds(1.5f);
         outOfOrder.GetComponent<Text>().enabled = false;
+    }
+
+    public IEnumerator CutScene()
+    {
+        cutSceneCam.SetActive(true);
+        player.SetActive(false);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(minigame);
     }
 }
