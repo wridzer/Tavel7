f6d42169206271e9ecd4226bb382cfd78db2b41a
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2021-01-31 04:06:34 +0100

    Golf level 4 (+ zand banken + hole 3 - hole 4;)

--- GGJ2021/Assets/Scripts/golf/Golfing.cs
+++ GGJ2021/Assets/Scripts/golf/Golfing.cs
@@ -8,6 +8,9 @@ public class Golfing : MonoBehaviour
     bool GolfHitCheck = false;
     public GameObject Mouse;
     public Rigidbody2D rb2D;
+    public GameObject Line;
+
+    [SerializeField] int AantalBinnen;
 
     [SerializeField]
     float distanceX;
@@ -16,10 +19,13 @@ public class Golfing : MonoBehaviour
     [SerializeField]
     int GolfPower;
 
+
     private void OnMouseDown()
     {
         if (rb2D.velocity.magnitude < 0.1)
-        { GolfHitCheck = true; }
+        {
+            GolfHitCheck = true;
+        }
     }
 
     private void OnMouseUp()
@@ -29,15 +35,49 @@ public class Golfing : MonoBehaviour
             distanceX = Mouse.transform.position.x - this.gameObject.transform.position.x ;
             distanceY = Mouse.transform.position.y - this.gameObject.transform.position.y ;
 
-            rb2D.AddForce(new Vector3(distanceX * Time.deltaTime * GolfPower,distanceY * Time.deltaTime * GolfPower ,0));
-
-            
-
+            rb2D.AddForce(new Vector3(Mathf.Clamp(distanceX,-4,4) * Time.deltaTime * GolfPower,Mathf.Clamp(distanceY,-4,4) * Time.deltaTime * GolfPower ,0));
         }
         GolfHitCheck = false; 
     }
 
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Terrain")
+        {
+            AantalBinnen++;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.tag == "Terrain")
+        {
+            AantalBinnen--;
+        }
+
+    }
+
     void Update()
     {
+        if (AantalBinnen > 0)
+        {
+            rb2D.drag = 30f;
+        }
+        else
+        {
+            rb2D.drag = 1.5f;
+        }
+
+        if (GolfHitCheck == true)
+        {   
+            Line.GetComponent<LineRenderer>().SetPosition(0, new Vector3 (Mathf.Clamp(Mouse.transform.position.x,-4 + this.gameObject.transform.position.x, 4 + this.gameObject.transform.position.x), Mathf.Clamp(Mouse.transform.position.y,-4 + this.gameObject.transform.position.y, 4 + this.gameObject.transform.position.y),0));
+            Line.GetComponent<LineRenderer>().SetPosition(1, this.gameObject.transform.position);
+        }
+        else
+        {
+            Line.GetComponent<LineRenderer>().SetPosition(0, new Vector3(0,0,0));
+            Line.GetComponent<LineRenderer>().SetPosition(1, new Vector3(0,0,0));
+        }
     }
 }
